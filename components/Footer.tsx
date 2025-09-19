4f536ab08f4029baad303c747c34ef5e99b0e683
Nazar Bychkar <mezismezis@gmail.com>
2025-09-19 12:22:09 +0300

    footer complete

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,133 +1,116 @@
+"use client";
+
+import { useTheme } from "@/lib/ThemeProvider";
+import { Montserrat } from "next/font/google";
+import Link from "next/link";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
 export default function Footer() {
+  const { isDark } = useTheme();
+
   return (
-    <footer className="w-full h-[972px] relative overflow-hidden">
-      <div className="w-full mx-1 h-52 left-[31px] top-[187px] absolute">
-        <div className="left-[9px] top-[-20px] absolute justify-center text-[222.70px] leading-[207.94px] tracking-[50px]">
+    <footer className="mt-50 m-10 h-[764px] relative overflow-hidden flex flex-col justify-between">
+      <div className="w-full mx-1 h-52">
+        <div
+          className={` ${montserrat.className} left-[9px] top-[-20px] absolute justify-center text-[209px] leading-[207.94px] tracking-[50px] border-b`}
+        >
           CHARS KYIV
         </div>
       </div>
-      <div className="w-96 h-28 left-[52px] top-[529.16px] absolute">
-        <div className="w-28 h-28 left-0 top-0 absolute">
-          <div className="w-28 h-28 left-0 top-0 absolute overflow-hidden">
-            <div className="w-28 h-28 left-0 top-0 absolute overflow-hidden">
-              <div className="w-28 h-28 left-[0.67px] top-[0.67px] absolute outline outline-[1.33px] outline-offset-[-0.67px] outline-stone-100" />
-              <div className="w-6 h-7 left-[42.37px] top-[39.37px] absolute bg-stone-100" />
-              <div className="w-2.5 h-2.5 left-[48.37px] top-[45.37px] absolute bg-stone-100" />
+
+      <div className="flex justify-between">
+        <div className="flex flex-col gap-4">
+          <div className="flex justify-start gap-4">
+            <div className="w-26 h-26 rounded-full border-1 flex items-center justify-center" />
+            <div className="w-56 h-11 text-xl flex justify-start my-3">
+              Адреса шоуруму:
+              <br />
+              Київ, вул. Костянтинівська, 21
+            </div>
+          </div>
+
+          <div className="flex justify-start gap-4">
+            <div className="w-26 h-26 rounded-full border-1 flex items-center justify-center" />
+            <div className="w-56 h-5  text-xl flex justify-start my-auto">
+              charskyiev91@gmail.com
             </div>
           </div>
         </div>
-        <div className="w-56 h-11 left-[132px] top-[30.37px] absolute justify-center text-stone-100 text-xl font-normal font-['Arial'] leading-normal">
-          Адреса шоуруму:
-          <br />
-          Київ, вул. Костянтинівська, 21
-        </div>
-      </div>
-      <div className="w-56 h-28 left-[586.41px] top-[529.16px] absolute">
-        <div className="w-28 h-28 left-0 top-0 absolute">
-          <div className="w-28 h-28 left-0 top-0 absolute overflow-hidden">
-            <div className="w-28 h-28 left-0 top-0 absolute overflow-hidden">
-              <div className="w-28 h-28 left-[0.67px] top-[0.67px] absolute outline outline-[1.33px] outline-offset-[-0.67px] outline-stone-100" />
-              <div className="w-7 h-7 left-[39.17px] top-[39.17px] absolute bg-stone-100" />
+
+        <div className="flex flex-col gap-4">
+          <div className="flex justify-start gap-4">
+            <div className="w-26 h-26 rounded-full border-1 flex items-center justify-center" />
+            <div className="w-32 h-11 text-xl flex justify-start my-auto">
+              Instagram
+            </div>
+          </div>
+
+          <div className="flex justify-start gap-4">
+            <div className="w-26 h-26 rounded-full border-1 flex items-center justify-center" />
+            <div className="w-32 h-5  text-xl flex justify-start my-auto">
+              Facebook
             </div>
           </div>
         </div>
-        <div className="w-24 h-5 left-[132.09px] top-[42.90px] absolute justify-center text-stone-100 text-xl font-normal font-['Arial'] leading-normal">
-          Instagram
-        </div>
-      </div>
-      <div className="w-96 h-28 left-[52px] top-[667.78px] absolute">
-        <div className="w-28 h-28 left-0 top-0 absolute">
-          <div className="w-28 h-28 left-0 top-0 absolute overflow-hidden">
-            <div className="w-28 h-28 left-0 top-0 absolute overflow-hidden">
-              <div className="w-28 h-28 left-[0.67px] top-[0.67px] absolute outline outline-[1.33px] outline-offset-[-0.67px] outline-stone-100" />
-              <div className="w-7 h-5 left-[40.03px] top-[42.70px] absolute outline outline-2 outline-offset-[-1px] outline-stone-100" />
-              <div className="w-4 h-1.5 left-[45.37px] top-[49.37px] absolute outline outline-2 outline-offset-[-1px] outline-stone-100" />
+
+        <div className="flex flex-col gap-22">
+          <div className="flex justify-start gap-4">
+            {/* <div className="w-28 h-28 rounded-full border-1 flex items-center justify-center" /> */}
+            <div className="w-56 h-11  flex flex-col justify-start my-auto">
+              <span className="text-2xl">Графік роботи:</span>
+              <span className="text-lg">Пн-Пт 13:00-19:00</span>
+            </div>
+          </div>
+
+          <div className="flex justify-start gap-4">
+            {/* <div className="w-28 h-28 rounded-full border-1 flex items-center justify-center" /> */}
+            <div className="w-56 h-11  flex flex-col justify-start my-auto">
+              <span className="text-2xl">Телефон</span>
+              <span className="text-lg">+380 5086 730 48</span>
             </div>
           </div>
         </div>
-        <div className="w-64 h-5 left-[132.09px] top-[42.91px] absolute justify-center text-stone-100 text-xl font-normal font-['Arial'] leading-normal">
-          charskyiev91@gmail.com
-        </div>
-      </div>
-      <div className="w-56 h-28 left-[586.41px] top-[667.78px] absolute">
-        <div className="w-28 h-28 left-0 top-0 absolute">
-          <div className="w-28 h-28 left-0 top-0 absolute overflow-hidden">
-            <div className="w-28 h-28 left-0 top-0 absolute overflow-hidden">
-              <div className="w-28 h-28 left-[0.67px] top-[0.67px] absolute outline outline-[1.33px] outline-offset-[-0.67px] outline-stone-100" />
-              <div className="w-3.5 h-6 left-[46.70px] top-[40.03px] absolute bg-stone-100" />
-            </div>
-          </div>
-        </div>
-        <div className="w-24 h-5 left-[132.09px] top-[42.91px] absolute justify-center text-stone-100 text-xl font-normal font-['Arial'] leading-normal">
-          Facebook
-        </div>
-      </div>
-      <div className="w-44 h-6 left-[961px] top-[549.53px] absolute justify-center text-stone-100 text-2xl font-normal font-['Arial'] leading-loose">
-        Графік роботи
-      </div>
-      <div className="left-[1278px] top-[546px] absolute justify-center text-stone-100 text-2xl font-normal font-['Arial'] leading-loose">
-        Навігація
-      </div>
-      <div className="w-40 h-4 left-[961px] top-[597.39px] absolute justify-center text-stone-100 text-lg font-normal font-['Arial'] leading-snug">
-        Пн-Пт 13:00-19:00
-      </div>
-      <div className="w-44 h-16 left-[961px] top-[686.50px] absolute">
-        <div className="w-28 h-6 left-0 top-[3px] absolute justify-center text-stone-100 text-2xl font-normal font-['Arial'] leading-loose">
-          Телефон
-        </div>
-        <div className="w-40 h-4 left-0 top-[50.86px] absolute justify-center text-stone-100 text-lg font-normal font-['Arial'] leading-snug">
-          +380 5086 730 48
-        </div>
-      </div>
-      <div className="w-[1820px] h-0 left-[52px] top-[381px] absolute outline outline-1 outline-offset-[-0.50px] outline-stone-100/50"></div>
-      <div
-        data-property-1="Default"
-        className="w-60 h-60 left-[1629px] top-[530px] absolute"
-      >
-        <div className="w-60 h-60 left-0 top-0 absolute bg-stone-100 rounded-full" />
-        <div className="left-[52px] top-[106px] absolute justify-center text-stone-900 text-2xl font-normal font-['Arial'] leading-loose">
-          На головну
-        </div>
-      </div>
-      <div className="w-40 left-[1278px] top-[595px] absolute inline-flex flex-col justify-start items-start gap-6">
-        <div
-          data-property-1="Default"
-          className="inline-flex justify-center items-center gap-2.5"
-        >
-          <div className="justify-center text-stone-100 text-lg font-normal font-['Arial'] leading-snug">
+
+        <div className="flex flex-col gap-3">
+          <span className="text-2xl">Навігація</span>
+          <Link href="/#about" className="text-lg">
             Про нас
-          </div>
-        </div>
-        <div
-          data-property-1="Default"
-          className="self-stretch inline-flex justify-center items-center gap-2.5"
-        >
-          <div className="justify-center text-stone-100 text-lg font-normal font-['Arial'] leading-snug">
+          </Link>
+          <Link href="/#payment-and-delivery" className="text-lg">
             Оплата і доставка
-          </div>
-        </div>
-        <div
-          data-property-1="Default"
-          className="inline-flex justify-center items-center gap-2.5"
-        >
-          <div className="justify-center text-stone-100 text-lg font-normal font-['Arial'] leading-snug">
+          </Link>
+          <Link href="/#reviews" className="text-lg">
             Відгуки
-          </div>
-        </div>
-        <div
-          data-property-1="Default"
-          className="inline-flex justify-center items-center gap-2.5"
-        >
-          <div className="justify-center text-stone-100 text-lg font-normal font-['Arial'] leading-snug">
+          </Link>
+          <Link href="/#contacts" className="text-lg">
             Контакти
-          </div>
+          </Link>
         </div>
+
+        <Link
+          href="/#"
+          className={`w-60 h-60 rounded-full flex justify-center ${
+            isDark ? "bg-stone-100" : "bg-stone-900"
+          }`}
+        >
+          <span
+            className={`my-auto text-2xl ${
+              isDark ? "text-stone-900" : "text-stone-100"
+            }`}
+          >
+            На головну
+          </span>
+        </Link>
       </div>
-      <div className="left-[53px] top-[919px] absolute justify-center text-stone-100 text-xl font-normal font-['Arial'] leading-loose">
-        Chars Kyiv © 2025 All rights reserved
-      </div>
-      <div className="left-[1614px] top-[919px] absolute justify-center text-stone-100 text-xl font-normal font-['Arial'] leading-loose">
-        Політика конфінденційності
+
+      <div className="flex justify-between">
+        <span className="text-xl text-stone-100">
+          Chars Kyiv © 2025 All rights reserved
+        </span>
+        <span className="text-xl text-stone-100">
+          Політика конфінденційності
+        </span>
       </div>
     </footer>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import MenuDrawer from "@/components/MenuDrawer";
 import Search from "@/components/Search";
 import ShoppingBasket from "@/components/ShoppingBasket";
 import { useTheme } from "@/lib/ThemeProvider";
+import Link from "next/link";
 
 export default function Header() {
   const { isDark, setIsDark } = useTheme();
@@ -12,7 +13,7 @@ export default function Header() {
   const toggleTheme = () => setIsDark((prev) => !prev);
   return (
     <header className="flex justify-between w-full h-20 relative  overflow-hidden">
-      <a href="/#">
+      <Link href="/#">
         <Image
           className="m-3 mx-10"
           height="57"
@@ -24,16 +25,16 @@ export default function Header() {
               : "/images/light-theme/chars-logo-header-light.png"
           }
         />
-      </a>
+      </Link>
 
       <div className="w-4xl inline-flex justify-start items-center gap-20">
         <div className="w-20 h-8 relative">
-          <a
+          <Link
             className="left-0 top-[3px] absolute justify-center text-xl font-normal font-['Inter']"
             href="/#about"
           >
             Про нас
-          </a>
+          </Link>
         </div>
         <div className="w-20 h-8 relative">
           <button
@@ -44,28 +45,28 @@ export default function Header() {
           </button>
         </div>
         <div className="w-48 h-8 relative">
-          <a
+          <Link
             className="left-0 top-[3px] absolute justify-center text-xl font-normal font-['Inter']"
             href="/#payment-and-delivery"
           >
             Оплата і доставка
-          </a>
+          </Link>
         </div>
         <div className="w-20 h-8 relative">
-          <a
+          <Link
             className="left-0 top-[3px] absolute justify-center text-xl font-normal font-['Inter']"
             href="/#reviews"
           >
             Відгуки
-          </a>
+          </Link>
         </div>
         <div className="w-24 h-8 relative">
-          <a
+          <Link
             className="left-0 top-[3px] absolute justify-center text-xl font-normal font-['Inter']"
             href="/#contacts"
           >
             Контакти
-          </a>
+          </Link>
         </div>
       </div>
 
