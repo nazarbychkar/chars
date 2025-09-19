77c8a438429458a894afe5410a45b86cc5bf28ad
Nazar Bychkar <mezismezis@gmail.com>
2025-09-19 20:49:08 +0300

    all links in a sidebar

--- components/layout/SidebarMenu.tsx
+++ components/layout/SidebarMenu.tsx
@@ -23,47 +23,51 @@ export default function SidebarMenu({
       )}
 
       <div
-        className={`fixed top-0 left-0 h-full w-64 ${
+        className={`fixed top-0 left-0 h-full w-128 ${
           isDark ? "bg-stone-900" : "bg-stone-100"
         }  shadow-md z-40 transform transition-transform duration-300 ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         }`}
       >
-        <nav className="p-6 space-y-4 text-2xl font-medium">
-          <Link href="#home" className="block hover:text-[#8C7461]">
-            Усі
-          </Link>
-          <Link href="#about" className="block hover:text-[#8C7461]">
+        <nav className="flex flex-col p-6 m-5 space-y-4 text-3xl gap-3">
+          <div className="flex justify-between">
+            <Link href="#home" className=" hover:text-[#8C7461]">
+              Усі
+            </Link>
+            <button className="cursor-pointer hover:text-[#8C7461]" onClick={() => setIsOpen(false)}>x</button>
+          </div>
+
+          <Link href="#about" className=" hover:text-[#8C7461]">
             Сезон -&gt;
           </Link>
-          <Link href="#services" className="block hover:text-[#8C7461]">
+          <Link href="#services" className=" hover:text-[#8C7461]">
             Жилетки
           </Link>
-          <Link href="#contact" className="block hover:text-[#8C7461]">
+          <Link href="#contact" className=" hover:text-[#8C7461]">
             Головні убори
           </Link>
-          <Link href="#contact" className="block hover:text-[#8C7461]">
+          <Link href="#contact" className=" hover:text-[#8C7461]">
             Пальта
           </Link>
-          <Link href="#contact" className="block hover:text-[#8C7461]">
+          <Link href="#contact" className=" hover:text-[#8C7461]">
             Лімітований одяг
           </Link>
-          <Link href="#contact" className="block hover:text-[#8C7461]">
+          <Link href="#contact" className=" hover:text-[#8C7461]">
             Спортивний одяг
           </Link>
-          <Link href="#contact" className="block hover:text-[#8C7461]">
+          <Link href="#contact" className=" hover:text-[#8C7461]">
             Футболки
           </Link>
-          <Link href="#contact" className="block hover:text-[#8C7461]">
+          <Link href="#contact" className=" hover:text-[#8C7461]">
             Майки
           </Link>
-          <Link href="#contact" className="block hover:text-[#8C7461]">
+          <Link href="#contact" className=" hover:text-[#8C7461]">
             Штани
           </Link>
-          <Link href="#contact" className="block hover:text-[#8C7461]">
+          <Link href="#contact" className=" hover:text-[#8C7461]">
             Сорочки
           </Link>
-          <Link href="#contact" className="block hover:text-[#8C7461]">
+          <Link href="#contact" className=" hover:text-[#8C7461]">
             Джинси
           </Link>
         </nav>
