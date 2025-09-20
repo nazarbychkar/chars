fb15ac42e060bbc27261292978a77c758c8b3ab3
Nazar Bychkar <mezismezis@gmail.com>
2025-09-20 11:38:54 +0300

    about us section

--- components/main-page/AboutUs.tsx
+++ components/main-page/AboutUs.tsx
@@ -1,3 +1,18 @@
 export default function AboutUs() {
-    return <section></section>
+  return (
+    <section className="w-[1920px] h-[625px] relative overflow-hidden flex flex-col items-center gap-15 justify-center">
+      <div className="text-[#8C7461] text-center justify-center text-5xl font-normal font-['Inter'] uppercase">
+        Про нас
+      </div>
+      <div className="w-[1743px] text-center justify-center text-3xl font-normal font-['Inter'] leading-13">
+        CHARS — український бренд чоловічого одягу, заснований у серпні 2023
+        року в Києві.<br />Ми з’явилися під час війни, коли майбутнє було
+        невизначеним, але мрії залишалися сильними.<br />Шиємо те, що самі хочемо
+        носити — без компромісів.<br />У власному цеху контролюємо кожен етап
+        виробництва, поєднуючи смарт-кежуал, кежуал-класик і трохи спорту.<br />
+        Створюємо одяг для різних чоловіків — стриманих і сміливих, класичних і
+        нестандартних.<br />Різні, але справжні.
+      </div>
+    </section>
+  );
 }
