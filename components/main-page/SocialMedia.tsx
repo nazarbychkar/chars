50b02c6592b0c1a2a14cb432e6f910941d7f48f1
Nazar Bychkar <mezismezis@gmail.com>
2025-09-26 14:56:59 +0300

    mobile social media section

--- components/main-page/SocialMedia.tsx
+++ components/main-page/SocialMedia.tsx
@@ -10,58 +10,64 @@ export default function SocialMedia() {
     // h-[977px]
     <section
       id="contacts"
-      className="max-w-[1920px] mx-auto w-full  relative overflow-hidden m-36"
+      className="max-w-[1920px] mx-auto w-full relative overflow-hidden lg:m-36"
     >
-      <div className="flex justify-center">
-        <div className="flex justify-center gap-7">
+      <div className="flex flex-col-reverse lg:flex-row justify-center">
+        <div className="flex justify-center gap-4 sm:gap-7">
           <img
-            className="w-80 h-[700.25px] rounded-[46.43px]"
+            className="w-44 h-[300px] sm:w-80 sm:h-[700.25px] rounded-[24px] sm:rounded-[46.43px]"
             src="https://placehold.co/323x700"
+            alt="image-1"
           />
-          {/* <img className="w-96 h-[725.39px]" src="https://placehold.co/352x725" /> */}
           <img
-            className="w-80 h-[709.92px] rounded-[53.20px]"
+            className="w-44 h-[300px] sm:w-80 sm:h-[709.92px] rounded-[24px] sm:rounded-[53.20px]"
             src="https://placehold.co/327x710"
+            alt="image-2"
           />
-          {/* <img className="w-96 h-[725.39px]" src="https://placehold.co/352x725" /> */}
         </div>
 
-        <div className="flex flex-col gap-10 ml-25 m-18">
+        <div className="flex flex-col gap-10 m-8 lg:ml-25 lg:m-18">
           <div className="flex flex-col">
-            <span className="text-stone-500 text-8xl font-normal font-['Inter']">
+            <span className="text-stone-500 text-5xl lg:text-8xl font-normal font-['Inter']">
               Ми ближче,{" "}
             </span>
-            <span className="text-8xl font-normal font-['Inter']">
+            <span className="text-5xl lg:text-8xl font-normal font-['Inter']">
               ніж здається!
             </span>
           </div>
 
-          <div className="w-[465px] justify-center text-3xl font-normal font-['Inter'] capitalize leading-9">
+          <div className="border-b lg:border-0 lg:w-[465px] justify-center text-lg lg:text-3xl font-normal font-['Inter'] capitalize leading-9">
             Лімітована колекція — для тих кому важлива унікальність.
           </div>
 
-          <div className="flex justify-between w-115 items-center">
+          <div className="flex justify-start gap-10 lg:justify-between items-center w-full lg:w-115">
             <Link
               href="https://www.tiktok.com/"
-              className={`w-80 h-16 text-center flex items-center ${
-                isDark ? "bg-stone-100 text-black" : "bg-stone-900 text-white "
-              } justify-center text-2xl font-normal font-['Inter'] uppercase leading-none tracking-tight`}
+              className={`w-60 h-12 md:w-80 md:h-16 text-center flex items-center ${
+                isDark ? "bg-stone-100 text-black" : "bg-stone-900 text-white"
+              } justify-center text-base md:text-2xl font-normal font-['Inter'] uppercase leading-none tracking-tight`}
             >
               МИ В TIKTOK
             </Link>
-            <img className="w-10 h-10" src="https://placehold.co/39x39" />
+            <img
+              className="w-8 h-8 md:w-10 md:h-10"
+              src="https://placehold.co/39x39"
+            />
           </div>
 
-          <div className="flex justify-between items-center w-115">
+          <div className="flex justify-start gap-10 lg:justify-between items-center w-full lg:w-115 mt-4 md:mt-0">
             <Link
               href="https://www.instagram.com/"
-              className={`w-80 h-16 text-center flex items-center ${
-                isDark ? "bg-stone-100 text-black" : "bg-stone-900 text-white "
-              } justify-center text-2xl font-normal font-['Inter'] uppercase leading-none tracking-tight`}
+              className={`w-60 h-12 md:w-80 md:h-16 text-center flex items-center ${
+                isDark ? "bg-stone-100 text-black" : "bg-stone-900 text-white"
+              } justify-center text-base md:text-2xl font-normal font-['Inter'] uppercase leading-none tracking-tight`}
             >
               МИ В ІНСТАГРАМ
             </Link>
-            <img className="w-10 h-10" src="https://placehold.co/39x39" />
+            <img
+              className="w-8 h-8 md:w-10 md:h-10"
+              src="https://placehold.co/39x39"
+            />
           </div>
         </div>
       </div>
